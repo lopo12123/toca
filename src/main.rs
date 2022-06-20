ae37e7784ec3c548ddb990560d126f14652e3236
chenmh <chenmh@sucsoft.com>
2022-06-20 18:04:44 +0800

    test: full test of hello

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ fn main() {
     let mut ins = Toca::new();
 
     ins.add_actions(&mut vec![
-        Action::MouseMove { delay: 2000, target: [2000, 1300] },
+        Action::MouseMoveAbsolute { delay: 2000, target: [2000, 1300] },
         Action::MouseLeft { delay: 100 },
         Action::KeyClick { delay: 100, key: Key::Layout('h') },
         Action::KeyClick { delay: 100, key: Key::Layout('e') },
         Action::KeyClick { delay: 100, key: Key::Layout('l') },
         Action::KeyClick { delay: 100, key: Key::Layout('l') },
         Action::KeyClick { delay: 100, key: Key::Layout('o') },
+        Action::KeyClick { delay: 100, key: Key::Return },
+        Action::KeyClick { delay: 100, key: Key::Return },
     ]);
 
     ins.play_actions();
