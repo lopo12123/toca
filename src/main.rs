63277b2c9cbf22e8deb73d3b65a284b1331f97a1
lopo <lopo@zju.edu.cn>
2022-07-05 16:50:15 +0800

    refactor: clear main

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,5 @@ use toca::{record::KeyboardRecorder, Keycode};
 
 #[allow(unused)]
 fn main() {
-    let mut recorder = KeyboardRecorder::new();
-
-    println!("record start.");
-    recorder.do_record(Keycode::Escape);
-    println!("record stop.");
-
-    for ev in recorder.get_record() {
-        println!("[{}ms]: {} {}", ev.timestamp, (if ev.press { "Press" } else { "Release" }), ev.code);
-    }
+    println!("nothing here, see examples.");
 }
