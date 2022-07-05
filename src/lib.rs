a2ca1c93d1a4946223f7e9f04db99fc74926cdb9
chenmh <chenmh@sucsoft.com>
2022-07-05 15:27:23 +0800

    refactor: move the global function to the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 pub mod record;
 pub mod display;
+
+/// javascript-like `setTimeout`
+pub fn set_timeout<T>(mut callback: T, ms: u64)
+    where T: FnMut() -> () {
+    sleep(Duration::from_millis(ms));
+    callback();
+}
