7e2f02175d9a82c5c7f2a140977b34aa095ca4c5
chenmh <chenmh@sucsoft.com>
2022-07-05 15:27:28 +0800

    refactor: move the global function to the lib

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ use std::{thread::sleep, time::Duration};
 use device_query::Keycode;
 use toca::record::KeyboardRecorder;
 
-/// 顶层模块下, 供子模块调用
-fn set_timeout<T>(mut callback: T, ms: u64)
-    where T: FnMut() -> () {
-    sleep(Duration::from_millis(ms));
-    callback();
-}
-
 #[allow(unused)]
 fn main() {
     let mut recorder = KeyboardRecorder::new();
