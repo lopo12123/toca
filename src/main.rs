7700236ad75db8d4381875cdf39b62adda489228
lopo <lopo@zju.edu.cn>
2022-07-06 16:40:30 +0800

    test: as union test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use toca::{record::KeyboardRecorder, Keycode};
+use enigo::KeyboardControllable;
+use toca::set_timeout;
 
 #[allow(unused)]
 fn main() {
     println!("nothing here, see examples.");
+
+    set_timeout(|| {
+        let mut en = enigo::Enigo::new();
+        en.key_click(enigo::Key::Layout('['));
+        en.key_click(enigo::Key::Layout(']'));
+        en.key_click(enigo::Key::Layout('-'));
+        en.key_click(enigo::Key::Layout('='));
+        en.key_click(enigo::Key::Return);
+    }, 4_000);
 }
