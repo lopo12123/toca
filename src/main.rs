9dfcde348c2032464adb86ca2a745e4fa6b30f79
lopo <lopo@zju.edu.cn>
2022-07-06 18:17:16 +0800

    bugs: Key::Layout need to in lowercase

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use enigo::KeyboardControllable;
+use device_query::Keycode;
+use enigo::{Key, KeyboardControllable};
+use toca::display::KeyboardPlayer;
+use toca::record::{KeyboardAction, KeyboardEv};
 use toca::set_timeout;
 
 #[allow(unused)]
 fn main() {
     println!("nothing here, see examples.");
 
+    // let mock_key_action = KeyboardAction {
+    //     evs: vec![KeyboardEv {
+    //         code: Keycode::A,
+    //         press: true,
+    //         timestamp: 4_000,
+    //     }],
+    //     till: 4_0000,
+    // };
+    // let mut player = KeyboardPlayer::new();
+    // player.load(mock_key_action);
+    // player.do_play();
+
+    let mut en = enigo::Enigo::new();
     set_timeout(|| {
-        let mut en = enigo::Enigo::new();
-        en.key_click(enigo::Key::Layout('['));
-        en.key_click(enigo::Key::Layout(']'));
-        en.key_click(enigo::Key::Layout('-'));
-        en.key_click(enigo::Key::Layout('='));
-        en.key_click(enigo::Key::Return);
-    }, 4_000);
+        en.key_click(Key::Layout('A'));
+    }, 3000);
 }
