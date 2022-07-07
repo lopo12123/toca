3348d089903491a3c66090264b5d3c5ecdfaccbf
lopo <lopo@zju.edu.cn>
2022-07-07 10:18:38 +0800

    refactor: drop main later

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,5 @@ use toca::set_timeout;
 
 #[allow(unused)]
 fn main() {
-    println!("nothing here, see examples.");
-
-    // let mock_key_action = KeyboardAction {
-    //     evs: vec![KeyboardEv {
-    //         code: Keycode::A,
-    //         press: true,
-    //         timestamp: 4_000,
-    //     }],
-    //     till: 4_0000,
-    // };
-    // let mut player = KeyboardPlayer::new();
-    // player.load(mock_key_action);
-    // player.do_play();
-
-    let mut en = enigo::Enigo::new();
-    set_timeout(|| {
-        en.key_click(Key::Layout('A'));
-    }, 3000);
+    println!("nothing here, see tests/examples.");
 }
