81de1818dca8ca622a85d291f242f418222a6864
lopo <lopo@zju.edu.cn>
2022-07-08 16:44:37 +0800

    examples: support `to_string`

--- examples/keyboard_record.rs
+++ examples/keyboard_record.rs
@@ -9,6 +9,10 @@ fn main() {
     let action = recorder.do_record(Keycode::Escape);
     println!("record stop. duration: {}ms", action.till);
 
+    // to_string
+    println!("in json:\n{:?}\n", action.to_string().unwrap());
+
+    // directly use
     for ev in action.evs {
         println!("[{}ms]: {} {}", ev.timestamp, (if ev.press { "Press" } else { "Release" }), ev.code);
     }
--- examples/mouse_record.rs
+++ examples/mouse_record.rs
@@ -9,6 +9,10 @@ fn main() {
     let action = recorder.do_record(Keycode::Escape);
     println!("record stop. duration: {}ms", action.till);
 
+    // to_string
+    println!("in json:\n{:?}\n", action.to_string().unwrap());
+
+    // directly use
     for ev in action.evs {
         println!("[{}ms]: {:?} at {:?}", ev.timestamp, ev.ev_name, ev.position);
     }
